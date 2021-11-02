dc73af12eab018ac2d472005b9fe8c584c66e5e9
WildFritz <maleekbeazer@gmail.com>
2021-11-02 15:21:48 -0400

    Icons
    Added icons to the buttons and text appears when you hover over the button

---
+++ FM/Assets/Scripts/UIScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+    
+
+    public void OnButtonHover(Text text)
+    {
+        text.enabled = true;
+    }
+
+    public void OffButtonHover(Text text)
+    {
+        text.enabled = false;
+    }
+}
