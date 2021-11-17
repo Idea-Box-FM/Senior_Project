7cfef8d8f1610484f7dfe165bbe45443f881237f
pnaatz <41494483+pnaatz@users.noreply.github.com>
2021-11-17 01:18:53 -0500

    Added comments

--- FM/Assets/Scripts/Persistance/Editor/UnityBuilderEditor.cs
+++ FM/Assets/Scripts/Persistance/Editor/UnityBuilderEditor.cs
@@ -3,15 +3,20 @@ using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEngine;
 
+/*Flower Box
+ * Programmer Patrick Naatz
+ * Intention: Make a Post Build Process to copy the simulations folder to the correct build path. when making a build from unity.
+ * 
+ * NOTE: THIS SCRIPT HAS TO BE IN THE EDITOR FOLDER OR IT WILL CAUSE ERRORS AT BUILD TIME
+ */ 
+
 public static class UnityBuilderEditor
 {
     [PostProcessBuild]
     static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
     {
+        //Todo maybe make the simulations path into a scriptables, I do not know if scriptables will even work here especially since they have to be accessed via Asset manager
         string path = Path.GetDirectoryName(pathToBuiltProject) + "\\" + "Simulations";
-        Debug.Log(path);
-        Debug.Log("Source" + Directory.GetCurrentDirectory() + "\\" + "Simulations");
-        //FileUtil.CopyFileOrDirectory(Directory.GetCurrentDirectory() + "\\" + "Simulations", path);
         FileUtil.CopyFileOrDirectory(Directory.GetCurrentDirectory() + "\\" + "Simulations", path);
     }
 }
