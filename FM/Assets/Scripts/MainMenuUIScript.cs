697e299e4d9a834eb9b3fd34d9fcb6e2505ea33d
WildFritz <maleekbeazer@gmail.com>
2021-12-06 17:28:31 -0500

    Online simulations
    This online simulations stuff sucks

--- FM/Assets/Scripts/MainMenuUIScript.cs
+++ FM/Assets/Scripts/MainMenuUIScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using System.Linq;
 using UnityEngine.SceneManagement;
 using TMPro;
 
@@ -34,11 +35,14 @@ public class MainMenuUIScript : MonoBehaviour
 
     [Header("Scroll List")]
     public GameObject gameManager;
-    public GameObject panel;
-    public string[] nameList;
+    public GameObject LocalPanel;
+    //public GameObject OnlinePanel;
+    public string[] localSimList;
+    public string[] onlineSimList;
     string currentItem;
     public GameObject itemTemplate;
-    GameObject s;
+    GameObject l;
+    GameObject o;
 
 
     string xml = ".XML";
@@ -56,6 +60,8 @@ public class MainMenuUIScript : MonoBehaviour
 
     private bool waited;
 
+    int simListLength;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -63,7 +69,8 @@ public class MainMenuUIScript : MonoBehaviour
         uploadButton.interactable = false;
         loginButton.interactable = false;
         validation.text = "";
-        nameList = FileManager.fileManager.localSimulations;
+        localSimList = FileManager.fileManager.localSimulations;
+        onlineSimList = FileManager.fileManager.onlineSimulations;
 
         selectedSim = 0;
 
@@ -80,21 +87,23 @@ public class MainMenuUIScript : MonoBehaviour
             loginButton.interactable = true;
         }
 
+        
 
-        selectedButton = panel.transform.GetChild(0).gameObject.transform.GetChild(selectedSim).gameObject;
+
+        selectedButton = LocalPanel.transform.GetChild(0).gameObject.transform.GetChild(selectedSim).gameObject;
         selectedButton.GetComponent<Button>().image.color = new Color(1, 1, 1, 1);
 
 
-        for (int i = 0; i < nameList.Length; i++)
+        for (int i = 0; i < simListLength; i++)
         {
             if (i == selectedSim)
             {
-                selectedButton = panel.transform.GetChild(0).gameObject.transform.GetChild(selectedSim).gameObject;
+                selectedButton = LocalPanel.transform.GetChild(0).gameObject.transform.GetChild(selectedSim).gameObject;
                 selectedButton.GetComponent<Button>().image.color = new Color(0, 1, 0, 1);
             }
             else
             {
-                selectedButton = panel.transform.GetChild(0).gameObject.transform.GetChild(i).gameObject;
+                selectedButton = LocalPanel.transform.GetChild(0).gameObject.transform.GetChild(i).gameObject;
                 selectedButton.GetComponent<Button>().image.color = new Color(1, 1, 1, 0);
             }
         }
@@ -124,40 +133,99 @@ public class MainMenuUIScript : MonoBehaviour
     public void UpdateList()
     {
 
-        nameList = FileManager.fileManager.localSimulations;
+        localSimList = FileManager.fileManager.localSimulations;
 
-        for (int i = 0; i < nameList.Length; i++)
+        onlineSimList = FileManager.fileManager.onlineSimulations;
+
+        int osListDifference = 0;
+
+
+
+        //for (int i = 0; i < onlineSimList.Length; i++)
+        //{
+        //    for (int j = 0; j < localSimList.Length; j++)
+        //    {
+        //        if (onlineSimList[i] == localSimList[j])
+        //        {
+        //            //onlineSimList[i] = null;
+                    
+        //            //simListLength--;
+        //            osListDifference++;
+        //        }
+        //    }
+        //}
+
+        onlineSimList = localSimList.Distinct().ToArray<string>();
+
+
+
+        for (int i = 0; i < localSimList.Length; i++)
         {
 
-            s = Instantiate(itemTemplate, panel.transform.GetChild(0).transform);
-            string simName = nameList[i].TrimEnd(xmlTrim);
-            s.transform.GetChild(0).GetComponent<TMP_Text>().text = simName;
+            l = Instantiate(itemTemplate, LocalPanel.transform.GetChild(0).transform);
+            string simName = localSimList[i].TrimEnd(xmlTrim);
+            l.transform.GetChild(0).GetComponent<TMP_Text>().text = simName;
+            l.transform.GetChild(0).GetComponent<TMP_Text>().color = Color.blue;
+            l.GetComponent<Button>().AddEventListener(i, ItemClickedL);
 
-            s.GetComponent<Button>().AddEventListener(i, ItemClicked);
 
         }
+
+        for (int i = 0; i < (onlineSimList.Length); i++)
+        {
+            if (onlineSimList[i] != null)
+            {
+                o = Instantiate(itemTemplate, LocalPanel.transform.GetChild(0).transform);
+                string simName = onlineSimList[i].TrimEnd(xmlTrim);
+                o.transform.GetChild(0).GetComponent<TMP_Text>().text = simName;
+                o.transform.GetChild(0).GetComponent<TMP_Text>().color = Color.black;
+                o.GetComponent<Button>().AddEventListener(i, ItemClickedO);
+            }
+
+
+        }
+
+
+
+
+
     }
 
     public void UnloadList()
     {
         //Debug.Log(nameList.Length);
-        for (int i = 0; i < nameList.Length; i++)
+        for (int i = 0; i < localSimList.Length; i++)
         {
-            Debug.Log(panel.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject);
-           Destroy(panel.transform.GetChild(0).gameObject.transform.GetChild(i).gameObject);
+            Debug.Log(LocalPanel.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject);
+           Destroy(LocalPanel.transform.GetChild(0).gameObject.transform.GetChild(i).gameObject);
         }
 
         UpdateList();
     }
 
-    void ItemClicked (int itemIndex)
+    void ItemClickedL (int itemIndex)
     {
-       // Debug.Log("Button " + itemIndex + " was clicked");
-        currentItem = nameList[itemIndex];
+        // Debug.Log("Button " + itemIndex + " was clicked");
+        currentItem = localSimList[itemIndex];  // + xml;
+
+        FileManager.fileManager.SelectFile(currentItem);
 
         selectedSim = itemIndex;
 
         
+        Debug.Log(currentItem);
+        //FileManager.fileManager.SelectFile(nameList[itemIndex]);
+    }
+    void ItemClickedO(int itemIndex)
+    {
+        // Debug.Log("Button " + itemIndex + " was clicked");
+        currentItem = onlineSimList[itemIndex];  // + xml;
+
+        FileManager.fileManager.SelectFile(currentItem);
+
+        selectedSim = itemIndex + localSimList.Length;
+
+
         Debug.Log(currentItem);
         //FileManager.fileManager.SelectFile(nameList[itemIndex]);
     }
