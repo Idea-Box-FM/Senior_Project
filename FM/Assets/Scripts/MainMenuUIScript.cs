6c5e6cf5fd2043d8a4a20585989b4951df7c04ca
pnaatz <41494483+pnaatz@users.noreply.github.com>
2022-01-26 16:22:10 -0500

    More fixes
    Fixed IBC issue
    Fixed selection glitch on startup

--- FM/Assets/Scripts/MainMenuUIScript.cs
+++ FM/Assets/Scripts/MainMenuUIScript.cs
@@ -168,6 +168,7 @@ public class MainMenuUIScript : MonoBehaviour
                 s.transform.GetChild(0).GetComponent<TMP_Text>().text = simName;
                 s.transform.GetChild(0).GetComponent<TMP_Text>().color = Color.blue;
                 s.GetComponent<Button>().AddEventListener(i, ItemClicked);
+                
             }
             else
             {
@@ -177,7 +178,9 @@ public class MainMenuUIScript : MonoBehaviour
                 s.transform.GetChild(0).GetComponent<TMP_Text>().color = Color.black;
                 s.GetComponent<Button>().AddEventListener(i, ItemClicked);
             }
-            
+
+            if (simList[i] == FileManager.fileManager.currentSimulation)
+                selectedSim = i;
         }
 
     }
