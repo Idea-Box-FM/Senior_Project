a6f965a4cd47f5cc2ad933eab5af4a0e12f3f26e
DRLdestroyer <dylanlavimodiere@gmail.com>
2022-01-27 17:33:35 -0500

    Avoidance of file saving

--- FM/Assets/Scripts/Audio/SaveLoadOptions.cs
+++ FM/Assets/Scripts/Audio/SaveLoadOptions.cs
@@ -5,38 +5,60 @@ using UnityEngine.UI;
 using TMPro;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using System;
 
 public class SaveLoadOptions : MonoBehaviour
 {
     //place this script on a manager gameobject and use it's methods
     //saves all prefs as strings, then parse them to get their values
-    //0th part of key is data type, 1st part is name, 2nd part is instance ID
+    //0th part of key is data type, 1st part is name, 2nd part is instance ID //# <- look for #
     //make sure to set default values within the UI component!
 
+    public int playerPrefAmount = 0;//how many player prefs are there?
     public List<string> prefKeys = new List<string>();
     public List<string> prefValues = new List<string>();
 
-    public List<int> instanceIDs = new List<int>();
+    public static List<int> instanceIDs = new List<int>();//!does this need to be static?
 
+    #region Setup Methods
     private void OnEnable()
     {
+        //DefaultPrefs();
         GetAllPrefs();//load options
-        DefaultPrefs();
+        //!load in all preferences to sliders and place in lists
     }
     private void OnDisable()
     {
+        //DefaultPrefs();
         SetAllPrefs();//save options
-        DefaultPrefs();
     }
-
-    // Start is called before the first frame update
     void Start()
     {
-        DefaultPrefs();
+        //DefaultPrefs();
+    }
+    #endregion
+    
+    private void Update()
+    {
+        
     }
 
-    //add/update an entry in the list
-    public void AddEntry(string newKey, string newValue)
+    private void DefaultPrefs()//Adds a placeholder entry
+    {
+        string key = "Int, Default, 0";
+        string value = "100";
+        int ID = 0;
+
+        //add a first entry
+        if (prefKeys.Contains(key) == false)
+            prefKeys.Add(key);//key
+        if (prefValues.Contains(value) == false)
+            prefValues.Add(value);//value
+        if (instanceIDs.Contains(ID) == false)
+            instanceIDs.Add(ID);//instance ID
+    }
+
+    public void AddEntry(string newKey, string newValue)//add/update an entry in the list
     {
         if (prefKeys.Count <= 0)//if there are no entries
         {
@@ -46,7 +68,16 @@ public class SaveLoadOptions : MonoBehaviour
         {
             if (prefKeys.Contains(newKey))//if an entry already exists under the same ID
             {
-                EntryUpdate(newKey, newValue);//update it
+                for (int i = 0; i < prefKeys.Count; i++)//loop through list
+                {
+                    if (prefKeys[i] == newKey)//if same key
+                    {
+                        prefValues[i] = newValue;//update it
+                        //Debug.Log("Updated key with value");
+                        Debug.Log(Time.realtimeSinceStartup + " -> Updated " + newKey + " key with " + newValue + " value");
+                        break;
+                    }
+                }
             }
             else//if there is not an entry
             {
@@ -54,31 +85,21 @@ public class SaveLoadOptions : MonoBehaviour
             }
         }
     }
-    private void EntryStore(string newKey, string newValue)
+
+    private void EntryStore(string newKey, string newValue)//add the entry into the proper lists
     {
         //add it to the lists
         prefKeys.Add(newKey);
         prefValues.Add(newValue);
 
+        playerPrefAmount++;
+
         //Debug.Log("Created key with value");
         Debug.Log(Time.realtimeSinceStartup + " -> Created " + newKey + " key with " + newValue.ToString() + " value");
     }
-    private void EntryUpdate(string newKey, string newValue)
-    {
-        for (int i = 0; i < prefKeys.Count; i++)//loop through list
-        {
-            if (prefKeys[i] == newKey)//if same key
-            {
-                prefValues[i] = newValue;//update it
-                //Debug.Log("Updated key with value");
-                //Debug.Log(Time.realtimeSinceStartup + " -> Updated " + newKey + " key with " + newValue + " value");
-                break;
-            }
-        }
-    }
 
-    //specific object's data entry methods
-    public void SetSliderPref(Slider slider)
+    #region Button Methods
+    public void SetSliderPref(Slider slider)//stores the slider's value
     {
         string newKey = "Float" + ", " + slider.name.Replace(",", "") + ", " + slider.GetInstanceID();//key has sections
         if(instanceIDs.Contains(slider.GetInstanceID()) == false)//if the instance is not already in the list
@@ -88,8 +109,7 @@ public class SaveLoadOptions : MonoBehaviour
         AddEntry(newKey, newValue);
     }
 
-    //specific object's data retrieval methods
-    public void GetSliderPref(Slider slider)//on load, apply the saved preferences to the sliders
+    public void GetSliderPref(Slider slider)//apply the saved preferences to the sliders
     {
         //apply the slider's stored preference when the screen is loaded to prevent it from being shown as the wrong value
         string getKey = "Float" + ", " + slider.name.Replace(",", "") + ", " + slider.GetInstanceID();
@@ -97,7 +117,7 @@ public class SaveLoadOptions : MonoBehaviour
         var prefs = GetAllPrefs();
 
         //checks order inversed to have most recent updated last
-        if (prefs.ContainsKey(getKey) == true)//if the key exists //!playerPrefs
+        if (prefs.ContainsKey(getKey) == true)//if the key exists //!use with playerPrefs
         {
             //update the value
             float newValue = float.Parse(prefs[getKey]);//parse needed to convert, otherwise specified cast not valid
@@ -113,9 +133,8 @@ public class SaveLoadOptions : MonoBehaviour
 
         Debug.Log(slider.name + " updated via " + lastUpdated);
     }
-
-    //preference management methods
-    public void SetAllPrefs()//primarily for UI's "save" button
+    
+    public void SetAllPrefs()//store the preferences in the list into the player prefs file
     {
         for (int i = 0; i < prefKeys.Count; i++)
         {
@@ -132,13 +151,14 @@ public class SaveLoadOptions : MonoBehaviour
                     PlayerPrefs.SetString(prefKeys[i], prefValues[i]);
                     break;
             }
-            //Debug.Log("Saved new " + stringSplit[0] + " with \"" + prefKeys[i] + "\"  key and \"" + prefValues[i].ToString() + "\" value");
+            Debug.Log("Saved new " + stringSplit[0] + " with \"" + prefKeys[i] + "\"  key and \"" + prefValues[i].ToString() + "\" value");
         }
+
         Debug.Log("Saved all preferences");
-        PlayerPrefs.Save();//save to disk
+        PlayerPrefs.Save();//save to disk//!not required?
     }
 
-    public void ResetAllPrefs()
+    public void ResetAllPrefs()//delete all preferences from all sources
     {
         //remove player preferences
         PlayerPrefs.DeleteAll();
@@ -146,36 +166,23 @@ public class SaveLoadOptions : MonoBehaviour
         prefKeys.Clear();
         prefValues.Clear();
         instanceIDs.Clear();
-        //remove file
-        //!
+        playerPrefAmount = 0;
         Debug.Log("Removed all preferences");
 
         //set default preferences here
         DefaultPrefs();
     }
-    private void DefaultPrefs()
-    {
-        string key = "Int, Default, 0";
-        string value = "100";
-        int ID = 0;
+    #endregion
 
-        //add a first entry
-        if(prefKeys.Contains(key) == false)
-            prefKeys.Add(key);//key
-        if (prefValues.Contains(value) == false)
-            prefValues.Add(value);//value
-        if (instanceIDs.Contains(ID) == false)
-            instanceIDs.Add(ID);//instance ID
-    }
-
-    public SortedList<string, string> GetAllPrefs()
+    public SortedList<string, string> GetAllPrefs()//grab the preferences from the player preferences file
     {
-        SortedList<string, string> loadedEntries = new SortedList<string, string>();
+        SortedList<string, string> loadedEntries = new SortedList<string, string>();//store return value
         //loadedEntries.Clear();//clear to refill with updated data
 
-        for (int i = 0; i < prefKeys.Count; i++)
+        //!loop through player preferences
+        for (int i = 0; i < prefKeys.Count; i++)//! how many entries are in playerprefs?
         {
-            string[] stringSplit = prefKeys[i].Split(',');//separate parts of key
+            string[] stringSplit = prefKeys[i].Split(',');//separate parts of key//#
             switch (stringSplit[0])//data type as a string
             {
                 case "Float"://if float data type
@@ -185,17 +192,25 @@ public class SaveLoadOptions : MonoBehaviour
                     loadedEntries.Add(/*Key*/prefKeys[i], /*Value*/PlayerPrefs.GetInt(prefKeys[i], int.Parse(prefValues[i])).ToString());
                     break;
                 case "String"://if string data type
-                    loadedEntries.Add(prefKeys[i], PlayerPrefs.GetString(prefKeys[i], prefValues[i]));
+                    loadedEntries.Add(/*Key*/prefKeys[i], /*Value*/PlayerPrefs.GetString(prefKeys[i], prefValues[i]));
                     break;
             }
             Debug.Log("Loaded " + stringSplit[0] + " \"" + prefKeys[i] + "\"  key and \"" + prefValues[i] + "\" value");
-
         }
-        //Debug.Log("Loaded all preferences");
+
+        for(int i = 0; i<loadedEntries.Count; i++)
+        {
+            AddEntry(loadedEntries.Keys[i], loadedEntries.Values[i]);
+        }
+
+        Debug.Log("Loaded all preferences");
 
         return loadedEntries;//return in case needed
     }
 
+    //!end
+
+    /*//file method (DO NOT USE)
     private void SaveFile()
     {
         string destination = Application.persistentDataPath + "/preferences0.dat";
@@ -216,7 +231,7 @@ public class SaveLoadOptions : MonoBehaviour
         string destination = Application.persistentDataPath + "/preferences0.dat";
         FileStream file;
 
-        if (File.Exists(destination)) 
+        if (File.Exists(destination))
             file = File.OpenRead(destination);
         else
         {
@@ -228,11 +243,11 @@ public class SaveLoadOptions : MonoBehaviour
         SortedList<string, string> data = (SortedList<string, string>)bf.Deserialize(file);
         file.Close();
 
-        if(data != null)
+        if (data != null)
         {
-            foreach(string key in data.Keys)
+            foreach (string key in data.Keys)
             {
-                if(prefKeys.Contains(key) == false)
+                if (prefKeys.Contains(key) == false)
                     prefKeys.Add(key);//get keys
 
                 string[] stringSplit = key.Split(',');//separate parts of key
@@ -244,13 +259,14 @@ public class SaveLoadOptions : MonoBehaviour
                 if (prefValues.Contains(value) == false
                     ) prefValues.Add(value);//get values
             }
-            
+
             Debug.Log("Loaded file");
         }
         else
         {
             Debug.Log("Could not parse data from file");
         }
-        
+
     }
+    */
 }
