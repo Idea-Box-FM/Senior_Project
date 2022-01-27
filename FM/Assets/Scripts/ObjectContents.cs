dbcaf306fa63c1ab3156d8149ea907452db2ac80
WildFritz <maleekbeazer@gmail.com>
2022-01-27 17:24:43 -0500

    progress on labeling system

--- FM/Assets/Scripts/ObjectContents.cs
+++ FM/Assets/Scripts/ObjectContents.cs
@@ -4,19 +4,56 @@ using UnityEngine;
 
 public class ObjectContents : MonoBehaviour
 {
+    //public static ObjectContents contents;
 
-    public static 
-    // Start is called before the first frame update
-    void Start()
+    //This is a enum for the 9 different Hazard types
+    public enum Contents { HealthHazard, Flammability, CompressedGas, Corrosive, Explosive, Oxidizers, Environmental, AcuteToxicity, Other, None};
+    [Tooltip("This variable is used to set the contents of the object")]
+    public Contents currentContent = Contents.None;
+
+
+    /// <summary>
+    /// This function is meant to change the contents of the object this script is on
+    /// </summary>
+    /// <param name="c">The Input for the is the index number of the Contents enum + 1 </param>
+    public void ChangeContents(int c)
     {
-        
+        switch(c)
+        {
+            case 1:
+                currentContent = Contents.HealthHazard;
+                break;
+            case 2:
+                currentContent = Contents.Flammability;
+                break;
+            case 3:
+                currentContent = Contents.CompressedGas;
+                break;
+            case 4:
+                currentContent = Contents.Corrosive;
+                break;
+            case 5:
+                currentContent = Contents.Explosive;
+                break;
+            case 6:
+                currentContent = Contents.Oxidizers;
+                break;
+            case 7:
+                currentContent = Contents.Environmental;
+                break;
+            case 8:
+                currentContent = Contents.AcuteToxicity;
+                break;
+            case 9:
+                currentContent = Contents.Other;
+                break;
+            case 10:
+                currentContent = Contents.None;
+                break;
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
 
 }
