e4a6778e84e6bc16eece7176777cf14f6f3e14ab
DRLdestroyer <dylanlavimodiere@gmail.com>
2022-02-23 16:02:24 -0500

    Destroy Sound Effect plays, Tutorial started

---
+++ FM/Assets/Scripts/UI Scripts/Tutorial.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Sequence{
+    public Image UIArrowImage;
+    public TextMeshProUGUI text;
+}
+
+public class Tutorial : MonoBehaviour
+{
+    List<Sequence> sequences = new List<Sequence>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
